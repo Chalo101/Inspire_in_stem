3f9a026ef7deff458710e5f6c414cdc4ff334095
Chalo101 <charlemuteithia@gmail.com>
2022-05-17 22:43:42 -0700

    lesson 2 on intagers

--- lesson4.py
+++ lesson4.py
@@ -25,3 +25,29 @@ PI = 3.142
 
 area = PI * radius * radius
 print ("the area of a circle is " +str (area))
+
+#volumeof a cube
+length = 7
+length = input(" Enter the length ")
+volumeOfACube = int(length) *int(length) *int(length)
+
+print(" The volume of a cube is " +str(volumeOfACube))
+
+#surface area of a cylinder
+
+PI = 3.142
+radius = 7
+diameter = 14
+height =3.5
+surfaceArea = 2* PI * radius + PI *diameter * height 
+
+print(" The surface area of the cylinder is") +str(surfaceArea)
+
+#Volume of a cylinder
+radius = 7
+height =4
+PI = 3.142
+Vol = int(PI)* int(radius) * int (radius) * int (height)
+
+print ("The volume of the cylinder is") +str(Vol)
+ 
