69e5dab9fad64e1cb98b81d4d3615fd98e28485a
Chalo101 <charlemuteithia@gmail.com>
2022-05-17 22:55:51 -0700

    lesson 2 on intagers

--- lesson4.py
+++ lesson4.py
@@ -18,7 +18,7 @@ print("the quotient of x and y is " +str (quot))
   # volume of a cylinder
 
 
-  # volume of a circle
+  #
 radius = input("Enter the radius of the circle " +str(7))
 radius = 7
 PI = 3.142
