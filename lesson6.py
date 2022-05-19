ac08363dbc140c383b889c9deb3f07fd04684cff
Chalo101 <charlemuteithia@gmail.com>
2022-05-19 00:47:44 -0700

    lesson 6

---
+++ lesson6.py
@@ -0,0 +1,32 @@
+# Learning about lists
+motorcycle_owner ="Mojo Jojo"
+motorcycle =['Honda' , 'Yamaha', 'Suzuki']
+
+# accessing list items using the index
+#print(motorcycle)
+#motorcycle[0]='Bugatti'#changing elements in a list
+#print(motorcycle)
+
+#motorcycle.append('Bugatti,Yonaha')
+#print(motorcycle)
+
+plate_number = ['H1234', 'y1234','g1234']
+#print(str(motorcycle[0]) + str(plate_number[0]))
+#print(str(motorcycle[1]) + str(plate_number[1]))
+#print(str(motorcycle[2])+ str(plate_number[2]))
+
+
+# Deleting an item from a list -->del
+# del motorcycle[0]
+# print(motorcycle)
+
+# popped_motorcycle = motorcycle.pop()
+# print(popped_motorcycle)
+
+#print a statement : My name is mojo Jojo and I own a motorcylce plate number
+#print("My name is " +str(motorcycle_owner)+ " and I own a " + str(motorcycle[0]) +" plate number " + str(plate_number[0]))
+# removing an item from a list
+
+motorcycle.remove('Honda')
+
+print(motorcycle)
