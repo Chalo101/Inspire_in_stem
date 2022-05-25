65237aeeca0652c277217ae2c01550f3dea68372
Chalo101 <charlemuteithia@gmail.com>
2022-05-25 09:36:23 +0300

    assignment4 on if statements

---
+++ assignment4.py
@@ -0,0 +1,5 @@
+acc_bal =(int(input ("Enter your acc balance")))
+
+if (int(acc_bal) > 100000 and int(acc_bal) < 200000):
+    acc_bal=acc_bal -25000
+    print("we have deducted KSH 25000 from your account balance")
---
+++ assignment5.py
@@ -0,0 +1,9 @@
+#!usr/bin/python
+vehicles = ["bmw" ,"audi" ,"toyota", "mercedes", "jeep"]
+
+# using a for loop to print all vehicles
+
+for vehicle in vehicles:
+    if (vehicle== "jeep"):
+     print (vehicle.upper())
+
