ba9f1eed390e824679bab47f9e5efceb5a6d3bf6
Chalo101 <charlemuteithia@gmail.com>
2022-05-31 10:28:23 +0300

    lesson on quadratic equations

---
+++ functions.py
@@ -0,0 +1,18 @@
+#function parameters
+x=4
+y=5
+def add_numbers (x,y):
+    add_numbers = x+y
+print("The sum of numbers",add_numbers)
+add_numbers (20,40)
+add_numbers (70,50)
+add_numbers (1,4)
+
+def products_numbers(x,y):
+    products_numbers =x*y
+    print("the product of numbers: ",products_numbers)
+products_numbers(2,5)
+products_numbers(3,6)
+products_numbers(78,14)
+products_numbers(45,1)
+products_numbers(36,4)
---
+++ quadratic_equation.py
@@ -0,0 +1,16 @@
+from cmath import sqrt
+import math
+a =int(input("Enter the coefficient of the first term: "))
+b =int(input("Enter the coefficient of the second term: "))
+c =int(input("Enter the constant: "))
+w =math.sqrt(b**2 - 4*a*c)
+# a is the coefficient of first term
+# b is the coefficient of the second term
+# c is the constant
+
+def find_roots(a,b,c):
+    y_1=( -b + w) /(2*a)
+
+    y_2=( -b - w) /(2*a)
+    print("the roots of the quadratic equation are",y_1 , y_2)
+find_roots(a,b,c)
