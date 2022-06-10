81920e785ea98c7acc6f4d621b87184146446cd8
Chalo101 <charlemuteithia@gmail.com>
2022-06-10 10:27:24 +0300

    Project on web_scrapping

---
+++ projects.py/palindrome.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+###############################
+#  name : Charles Kagoko
+#  file : palindrome project using tkinter
+#  date : 8/6/2022
+###############################
+
+print("Welcome to the palindrome program")
+ipt=int(input("Do you want to enter a number"))
+if ipt[0] and ipt[-1]==ipt:
+   print("yes")
+
+
